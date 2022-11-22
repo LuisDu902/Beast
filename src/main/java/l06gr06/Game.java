fd3aa3e7bb39c7bf7c2b2dab52176e7063c36780
Athos Freitas <athos.coghi@gmail.com>
2022-11-22 11:51:07 +0000

    base game implementation

---
+++ src/main/java/l06gr06/Game.java
@@ -0,0 +1,29 @@
+package l06gr06;
+
+//import org.l06gr06.gui.LanternaGUI;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class Game {
+    private final LanternaGUI gui;
+    private State state;
+
+    public Game() throws FontFormatException, IOException, URISyntaxException {
+        this.gui = new LanternaGUI(50, 20);
+        this.state = new MenuState(new Menu());
+    }
+
+    public static void main(String[] args) throws IOException, FontFormatException, URISyntaxException {
+        new Game().start();
+    }
+
+    public void setState(State state) {
+        //to be implemented...
+    }
+
+    private void start() throws IOException {
+        //to be implemented...
+    }
+}
