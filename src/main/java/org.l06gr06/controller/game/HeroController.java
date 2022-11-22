e33a44da3cc1b5a9106749ae6a7c63174f5f9c91
Du <luispedro.2003@hotmail.com>
2022-11-22 12:32:19 +0000

    Starting controller implementation

---
+++ src/main/java/org.l06gr06/controller/game/HeroController.java
@@ -0,0 +1,45 @@
+package org.l06gr06.controller.game;
+
+import org.l06gr06.Game;
+/*
+import org.l06gr06.gui.GUI;
+import org.l06gr06.model.Position;
+import org.l06gr06.model.game.arena.Arena;
+*/
+
+public class HeroController extends GameController {
+    public HeroController(Arena arena) {
+        super(arena);
+    }
+
+    public void moveHeroLeft() {
+        moveHero(getModel().getHero().getPosition().getLeft());
+    }
+
+    public void moveHeroRight() {
+        moveHero(getModel().getHero().getPosition().getRight());
+    }
+
+    public void moveHeroUp() {
+        moveHero(getModel().getHero().getPosition().getUp());
+    }
+
+    public void moveHeroDown() {
+        moveHero(getModel().getHero().getPosition().getDown());
+    }
+
+    private void moveHero(Position position) {
+        if (getModel().isEmpty(position)) {
+            getModel().getHero().setPosition(position);
+            if (getModel().isMonster(position)) getModel().getHero().decreaseEnergy();
+        }
+    }
+
+    @Override
+    public void step(Game game, GUI.ACTION action, long time) {
+        if (action == GUI.ACTION.UP) moveHeroUp();
+        if (action == GUI.ACTION.RIGHT) moveHeroRight();
+        if (action == GUI.ACTION.DOWN) moveHeroDown();
+        if (action == GUI.ACTION.LEFT) moveHeroLeft();
+    }
+}
